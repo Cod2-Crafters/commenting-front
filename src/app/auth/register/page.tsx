e6660413ea9c9204eeba9eb699b9ea8e9212db7d
salkizzang <choiwsx@gmail.com>
2024-05-08 00:00:31 +0900

    회원가입 폼 추가

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "@/components/auth/register-form";
+
+const RegusterPage = () => {
+    return (
+        <RegisterForm />
+    );
+}
+
+export default RegusterPage;
--- src/components/auth/card-wrapper.tsx
+++ src/components/auth/card-wrapper.tsx
@@ -2,6 +2,8 @@
 
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { Header } from "@/components/auth/header";
+import { Social } from "@/components/auth/social";
+import { BackButton } from "@/components/auth/back-button";
 
 interface CardWrapperProps {
     children: React.ReactNode;
@@ -27,6 +29,18 @@ export const CardWrapper = ({
             <CardContent>
                 {children}
             </CardContent>
+            {showSocial &&
+                (
+                    <CardFooter>
+                        <Social />
+                    </CardFooter>
+                )}
+            <CardFooter>
+                <BackButton
+                    label={backButtonLabel}
+                    href={backButtonHref}
+                />
+            </CardFooter>
         </Card>
     )
 }
