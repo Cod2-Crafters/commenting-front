1d1852cd8d0ad50b97f6dd36c68f01384d3fadda
타스씨 <securemem@icloud.com>
2024-07-28 09:02:39 +0900

    chore: 설정 파일 추가 및 동기화

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   webpack: (config, { webpack }) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
     return config
+  },
+  async headers() {
+    return [
+      {
+        // CORS 설정을 적용할 경로 (모든 경로에 적용)
+        source: '/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
   },
 }
 
