7004d2c02a3c8f721c8391a0dbd794348c018343
타스씨 <securemem@icloud.com>
2024-07-28 09:06:15 +0900

    chore: 누락된 로그인 소스 추가

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,15 +1,15 @@
-import { login } from "@/lib/login";
-import { NextRequest, NextResponse } from "next/server";
+import { login } from '@/lib/login'
+import { NextRequest, NextResponse } from 'next/server'
 
+export async function POST(req: NextRequest) {
+  const { userid, email, token } = await req.json()
 
-export async function POST(req: NextRequest){
-    const { email, token } = await req.json();
+  const loginFormData = new FormData()
+  loginFormData.append('userid', userid)
+  loginFormData.append('email', email)
+  loginFormData.append('token', token)
 
-    const loginFormData = new FormData();
-    loginFormData.append("email", email);
-    loginFormData.append("token", token);
-    
-    await login(loginFormData);
+  await login(loginFormData)
 
-    return NextResponse.json({message : "로그인 성공"});
+  return NextResponse.json({ message: '로그인 성공' })
 }
