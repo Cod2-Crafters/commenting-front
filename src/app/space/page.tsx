56b084fa17f4854ed79d35f98617547fbd195a8e
타스씨-L <securemem@icloud.com>
2024-08-22 22:20:50 +0900

    feat: /space 접속 시, /space/{id}로 redirect

---
+++ src/app/space/page.tsx
@@ -0,0 +1,25 @@
+'use server'
+import { getSession } from '@/lib/login';
+import { RootState } from '@/store';
+import { redirect, useRouter } from 'next/navigation';
+import { useSelector } from 'react-redux';
+
+const SpacePage = async () => {
+
+    const session = await getSession();
+    console.log('my session', session);
+
+
+    if (!session)
+    {
+        redirect('/auth/login');
+    }
+    else
+    {
+        redirect(`/space/${session.user.id}`);
+    }
+  // 컴포넌트는 리디렉션을 위해 실제로 렌더링되지 않습니다.
+  return null;
+};
+
+export default SpacePage;
